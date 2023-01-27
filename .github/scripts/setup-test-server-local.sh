94ed930585ff9fb8805a0571c6e08e2ee117d962
Stan Brubaker <stan@brubaker.ws>
2023-01-27 14:39:51 -0700

    Updaate known hosts for ssh

--- .github/scripts/setup-test-server-local.sh
+++ .github/scripts/setup-test-server-local.sh
@@ -3,9 +3,10 @@ HOST=$1
 USER=$2
 PRIVATE_KEY=$3
 SCRIPTS_DIR=$4
-PRIVATE_FILE=~/.ssh/id_benchmark_workflow
+PRIVATE_FILE=~/.ssh/id_ed25519
 
 mkdir -p ~/.ssh/
 echo "${PRIVATE_KEY}" > ${PRIVATE_FILE}
 sudo chmod 600 ${PRIVATE_FILE}
-ssh -i ${PRIVATE_FILE} ${USER}@${HOST} 'bash -s' < ${SCRIPTS_DIR}/setup-test-server-remote.sh
+ssh-keyscan -H ${HOST} > ~/.ssh/known_hosts
+ssh ${USER}@${HOST} 'bash -s' < ${SCRIPTS_DIR}/setup-test-server-remote.sh
