0e92180b39fa0da95ddd48b30dec1e3284b02e6b
Stan Brubaker <stan@brubaker.ws>
2023-01-27 14:22:03 -0700

    More path issues

--- .github/scripts/setup-test-server-local.sh
+++ .github/scripts/setup-test-server-local.sh
@@ -2,9 +2,10 @@
 HOST=$1
 USER=$2
 PRIVATE_KEY=$3
-PRIVATE_FILE=~/.ssh/id_workflow
+SCRIPTS_DIR=$4
+PRIVATE_FILE=~/.ssh/id_benchmark_workflow
 
 mkdir -p ~/.ssh/
 echo "${PRIVATE_KEY}" > ${PRIVATE_FILE}
 sudo chmod 600 ${PRIVATE_FILE}
-ssh -i ${PRIVATE_FILE} ${USER}@${HOST} 'bash -s' < ./setup-test-server-remote.sh
+ssh -i ${PRIVATE_FILE} ${USER}@${HOST} 'bash -s' < ${SCRIPTS_DIR}/setup-test-server-remote.sh
