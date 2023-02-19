24b1dc6b9ea55ce839c41c75ef71b736260d6db2
stanbrub <stanbrubaker@illumon.com>
2023-02-18 21:27:41 -0700

    Reworked main class to remove launcher defaults

--- src/main/java/io/deephaven/benchmark/run/BenchmarkMain.java
+++ src/main/java/io/deephaven/benchmark/run/BenchmarkMain.java
@@ -2,36 +2,26 @@
 package io.deephaven.benchmark.run;
 
 import java.nio.file.Paths;
-import java.util.*;
 import org.junit.platform.console.ConsoleLauncher;
 import io.deephaven.benchmark.api.Bench;
 
 public class BenchmarkMain {
     static public void main(String[] args) {
+        System.exit(main1(args));
+    }
+
+    static public int main1(String[] args) {
         setSystemProperties();
         int exitCode = ConsoleLauncher.execute(System.out, System.err, args).getExitCode();
-        new ResultSummary(Paths.get(Bench.rootOutputDir)).summarize();
-        System.exit(exitCode);
+        if (exitCode == 0) {
+            new ResultSummary(Paths.get(Bench.rootOutputDir)).summarize();
+        }
+        return exitCode;
     }
 
     // Set system properties for running from the command line
     static void setSystemProperties() {
         System.setProperty("timestamp.test.results", "true");
-    }
-
-    static void addDefault(List<String> args, String shortSwitch, String longSwitch, String defaultValue) {
-        if (foundShortSwitch(args, shortSwitch) || foundLongSwitch(args, longSwitch))
-            return;
-        args.add(shortSwitch);
-        args.add(defaultValue);
-    }
-
-    static boolean foundShortSwitch(List<String> args, String shortSwitch) {
-        return (args.indexOf(shortSwitch) >= 0);
-    }
-
-    static boolean foundLongSwitch(List<String> args, String longSwitch) {
-        return args.stream().anyMatch(a -> a.startsWith(longSwitch));
     }
 
 }
