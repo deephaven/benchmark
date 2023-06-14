ca18cf1c6b49c13f78763a0ac29607ac70ebf058
stanbrub <stanbrubaker@illumon.com>
2023-06-14 17:01:13 -0600

    Scale datetime utils

--- src/it/java/io/deephaven/benchmark/tests/standard/formula/DateTimeFormulaTest.java
+++ src/it/java/io/deephaven/benchmark/tests/standard/formula/DateTimeFormulaTest.java
@@ -14,41 +14,35 @@ public class DateTimeFormulaTest {
     @Test
     public void now() {
         setup(6, 40, 5);
-        var q = "source.update(formulas=['New1 = now()'])"; // 0.25.0
-        // var q = "source.update(formulas=['New1 = currentTime()'])"; // 0.24.2
+        var q = "source.update(formulas=['New1 = now()'])";
         runner.test("Now- now()", q, "int250");
     }
 
     @Test
     public void parseInstant() {
         setup(1, 1, 1);
-        var q = "source.update(formulas=['New1 = parseInstant(`2023-05-31T04:52:14.001 ET`)'])"; // 0.25.0
-        // var q = "source.update(formulas=['New1 = convertDateTime(`2023-05-31T04:52:14.001 ET`)'])"; // 0.24.2
+        var q = "source.update(formulas=['New1 = parseInstant(`2023-05-31T04:52:14.001 ET`)'])";
         runner.test("ParseInstant- parseInstant(String)", q, "int250");
     }
 
     @Test
     public void parseDuration() {
-        setup(2, 5, 1);
-        var q = "source.update(formulas=['New1 = parseDuration(`PT4H52M14S`)'])"; // 0.25.0
-        // var q = "source.update(formulas=['New1 = convertPeriod(`T4H52M14S`)'])"; // 0.24.2
+        setup(3, 10, 2);
+        var q = "source.update(formulas=['New1 = parseDuration(`PT4H52M14S`)'])";
         runner.test("ParseDuration- parseDuration(String)", q, "int250");
     }
 
     @Test
     public void parseLocalTime() {
         setup(3, 10, 1);
-        var q = "source.update(formulas=['New1 = parseLocalTime(`04:52:14.001`)'])"; // 0.25.0
-        // var q = "source.update(formulas=['New1 = convertTime(`04:52:14.001`)'])"; // 0.24.2
+        var q = "source.update(formulas=['New1 = parseLocalTime(`04:52:14.001`)'])";
         runner.test("ParseLocalTime- parseLocalTime(String)", q, "int250");
     }
 
     @Test
     public void epochNanosToZonedDateTime() {
         setup(3, 8, 5);
-        var q = "source.update(formulas=['New1 = epochNanosToZonedDateTime(1000000, java.time.ZoneId.systemDefault())'])"; // 0.25.0
-        // var q = "source.update(formulas=['New1 = makeZonedDateTime(1000000, java.time.ZoneId.systemDefault())'])"; //
-        // 0.24.2
+        var q = "source.update(formulas=['New1 = epochNanosToZonedDateTime(1000000, java.time.ZoneId.systemDefault())'])";
         runner.test("EpochNanosToZonedDateTime- epochNanosToZonedDateTime(long, ZoneId)", q, "int250");
     }
 
