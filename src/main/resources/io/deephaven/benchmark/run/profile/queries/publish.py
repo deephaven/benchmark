a9c8e42cd83a255084835875eebc1ebfef0d1193
stanbrub <stanbrubaker@illumon.com>
2023-10-09 12:37:17 -0600

    Changed /data to /nfs for queries where appropriate

--- src/main/resources/io/deephaven/benchmark/run/profile/queries/publish.py
+++ src/main/resources/io/deephaven/benchmark/run/profile/queries/publish.py
@@ -5,12 +5,16 @@
 # Requirements: Deephaven 0.23.0 or greater
 
 from urllib.request import urlopen; import os
-root = 'file:///data' if os.path.exists('/data/deephaven-benchmark') else 'https://storage.googleapis.com'
+root = 'file:///nfs' if os.path.exists('/nfs/deephaven-benchmark') else 'https://storage.googleapis.com'
 with urlopen(root + '/deephaven-benchmark/benchmark_tables.dh.py') as r:
     benchmark_storage_uri_arg = root + '/deephaven-benchmark'
     benchmark_category_arg = 'nightly'  # release | nightly    
     benchmark_max_runs_arg = 45  # Latest X runs to include   
     exec(r.read().decode(), globals(), locals())
+
+import statistics
+def zscore(rate, rates):
+    return (rate - statistics.mean(rates)) / statistics.pstdev(rates)
 
 platform_details = bench_platforms.sort_descending(['run_id']).group_by(['run_id']).first_by().ungroup()
 
@@ -28,7 +32,7 @@ nightly_worst_rate_change = bench_results.where([
 ]).update([
     'past_5_rates_ex=op_rate_[i].subVector(1,6)','past_5_rates_in=op_rate','op_rate=op_rate[0]',
     'avg_rate_ex=avg(past_5_rates_ex)','var_rate_ex=rstd(past_5_rates_ex)',
-    'avg_rate_in=avg(past_5_rates_in)'
+    'avg_rate_in=avg(past_5_rates_in)','score=(float)zscore(op_rate,past_5_rates_ex)'
 ]).group_by([
     'benchmark_name','origin'
 ]).view([
@@ -36,9 +40,10 @@ nightly_worst_rate_change = bench_results.where([
     'Variability=(float)var_rate_ex[0]/100',
     'Rate=op_rate[0]',
     'Change=(float)gain(avg_rate_ex[0],op_rate[0])/100',
-    'Since_Release=(float)gain(avg_rate_in[1],op_rate[0])/100'
+    'Since_Release=(float)gain(avg_rate_in[1],op_rate[0])/100',
+    'Score=score[0]'
 ]).sort([
-    'Change'
+    'Score'
 ])
 
 nightly_worst_rate_change_large = nightly_worst_rate_change.head_by(20).format_columns([
@@ -48,9 +53,8 @@ nightly_worst_rate_change_large = nightly_worst_rate_change.head_by(20).format_c
 
 nightly_worst_rate_change_small = nightly_worst_rate_change.head_by(10).view([
     'Static_Benchmark=Static_Benchmark.substring(0, 20)+`...`',
-    'Chng5d=Change','Var5d=Variability','Rate','RlsChng=Since_Release'
+    'Chng5d=Change','Var5d=Variability','Rate','ChngRls=Since_Release'
 ]).format_columns([
     'Rate=Decimal(`###,##0`)','Chng5d=Decimal(`0.0%`)','Var5d=Decimal(`0.0%`)',
     'ChngRls=Decimal(`0.0%`)'
 ])
-
