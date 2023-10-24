6d9cb8083ed4b34775ffb49c1bdb0a89f7919ea2
stanbrub <stanbrubaker@illumon.com>
2023-10-23 23:19:34 -0600

    Moved select-distinct to its own package

--- src/it/java/io/deephaven/benchmark/tests/standard/select/SelectDistinctTest.java
+++ src/it/java/io/deephaven/benchmark/tests/standard/select/SelectDistinctTest.java
@@ -1,5 +1,5 @@
 /* Copyright (c) 2022-2023 Deephaven Data Labs and Patent Pending */
-package io.deephaven.benchmark.tests.standard.sort;
+package io.deephaven.benchmark.tests.standard.select;
 
 import org.junit.jupiter.api.*;
 import io.deephaven.benchmark.tests.standard.StandardTestRunner;
