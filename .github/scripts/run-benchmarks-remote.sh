eacee94190fb5dd9cbab66aa684e96e02614ab09
stanbrub <stanbrubaker@illumon.com>
2024-01-23 17:01:47 -0700

    First pass at updating scripts and workflows with adhoc ui variables

--- .github/scripts/run-benchmarks-remote.sh
+++ .github/scripts/run-benchmarks-remote.sh
@@ -7,26 +7,33 @@ set -o nounset
 # Run benchmarks on the remote side
 # Assumes the deephaven-benchmark-*.jar artifact has been built and placed
 
-if [[ $# != 1 ]]; then
-  echo "$0: Missing run type argument"
+if [[ $# != 4 ]]; then
+  echo "$0: Missing run type, test package, test regex, or row count argument"
   exit 1
 fi
 
 RUN_TYPE=$1
+TEST_PKG=$2
+TEST_RGX=$3
+ROW_COUNT=$4
 HOST=`hostname`
 RUN_DIR=/root/run
 DEEPHAVEN_DIR=/root/deephaven
 
 # Match run type (nightly, release, compare, adhoc) to benchmark test package
+# Note: Default TEST_PATTERN taken from default -n option in Junit Platform Console Standalone
 case ${RUN_TYPE} in
   adhoc)
-    TEST_PACKAGE=io.deephaven.benchmark.tests.standard.aggby
+    TEST_PACKAGE=${TEST_PKG}
+    TEST_PATTERN=${TEST_RGX}
     ;;
   compare)
     TEST_PACKAGE=io.deephaven.benchmark.tests.compare
+    TEST_PATTERN="^(Test.*|.+[.$]Test.*|.*Tests?)$"
     ;;
   *)
     TEST_PACKAGE=io.deephaven.benchmark.tests.standard
+    TEST_PATTERN="^(Test.*|.+[.$]Test.*|.*Tests?)$"
     ;;
 esac
 
@@ -47,8 +54,10 @@ rm -f data/*.*
 docker compose up -d
 sleep 10
 
+title "-- Running Benchmarks --"
 cd ${RUN_DIR}
-java -Dbenchmark.profile=${RUN_TYPE}-scale-benchmark.properties -jar deephaven-benchmark-*.jar -cp standard-tests.jar -p ${TEST_PACKAGE}
+cat ${RUN_TYPE}-scale-benchmark.properties | sed 's|${baseRowCount}|'"${ROW_COUNT}|g" > scale-benchmark.properties
+java -Dbenchmark.profile=scale-benchmark.properties -jar deephaven-benchmark-*.jar -cp standard-tests.jar -p ${TEST_PACKAGE} -n "${TEST_PATTERN}"
 
 title "-- Getting Docker Logs --"
 mkdir -p ${RUN_DIR}/logs
