0d2cbf38c3522a77b2e51815ee0a958582aae503
stanbrub <stanbrubaker@illumon.com>
2024-06-18 19:24:50 -0600

    Added tagged iterations

---
+++ .github/scripts/base62.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+# Convert the given number to base62 characters
+# ex. base62.sh 1718738365297350992 
+# ex. base62.sh $(date +%s%N) 
+
+DECNUM=$1
+
+BASE62=($(echo {0..9} {a..z} {A..Z}))
+for i in $(bc <<< "obase=62; ${DECNUM}"); do
+  echo -n ${BASE62[$(( 10#$i ))]}
+done && echo
+
+SCRIPT_DIR=$(temp=$(realpath "$0") && dirname "$temp")
+echo $SCRIPT_DIR
--- .github/scripts/fetch-results-local.sh
+++ .github/scripts/fetch-results-local.sh
@@ -7,17 +7,18 @@ set -o nounset
 # Fetches Benchmark results and logs from the remote test server and
 # compresses the runs before upload
 
+if [[ $# != 6 ]]; then
+  echo "$0: Missing host, user, run type, script dir, actor, or run label arguments"
+  exit 1
+fi
+
 HOST=$1
 USER=$2
-RUN_TYPE=$3
-ACTOR=${4:-}
-RUN_LABEL=${5:-}
+SCRIPT_DIR=$3
+RUN_TYPE=$4
+ACTOR=$5
+RUN_LABEL=${5:-$(${SCRIPT_DIR}/base62.sh $(date +%s%N))}
 RUN_DIR=/root/run
-
-if [[ $# != 3 ]] && [[ $# != 5 ]]; then
-    echo "$0: Missing host, user, run type, actor, or run label arguments"
-    exit 1
-fi
 
 # Pull results from the benchmark server
 scp -r ${USER}@${HOST}:${RUN_DIR}/results .
@@ -25,15 +26,8 @@ scp -r ${USER}@${HOST}:${RUN_DIR}/logs .
 scp -r ${USER}@${HOST}:${RUN_DIR}/*.jar .
 
 # If the RUN_TYPE is adhoc, userfy the destination directory
-DEST_DIR=${RUN_TYPE}
-if [ "${RUN_TYPE}" = "adhoc" ]; then
-    if [ -z "${ACTOR}" ] || [ -z "${RUN_LABEL}" ]; then
-        echo "$0: Missing actor of run label argument for adhoc run type"
-        exit 1
-    fi
-    DEST_DIR=${RUN_TYPE}/${ACTOR}/${RUN_LABEL}
-    mkdir -p ${DEST_DIR}
-fi
+DEST_DIR=${RUN_TYPE}/${ACTOR}/${RUN_LABEL}
+mkdir -p ${DEST_DIR}
 
 rm -rf ${DEST_DIR}
 mv results/ ${DEST_DIR}/
@@ -51,8 +45,8 @@ rm -rf ${TMP_SVG_DIR}
 # Compress CSV and Test Logs
 for runId in `find ${DEST_DIR}/ -name "run-*"`
 do
-    (cd ${runId}; gzip *.csv)
-    (cd ${runId}/test-logs; tar -zcvf test-logs.tgz *; mv test-logs.tgz ../)
-    rm -rf ${runId}/test-logs/
+  (cd ${runId}; gzip *.csv)
+  (cd ${runId}/test-logs; tar -zcvf test-logs.tgz *; mv test-logs.tgz ../)
+  rm -rf ${runId}/test-logs/
 done
 
