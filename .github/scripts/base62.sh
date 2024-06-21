07a66d61b35883106bd99a67beb93a24ffacd3ed
stanbrub <stanbrubaker@illumon.com>
2024-06-20 21:45:00 -0600

    User base 62 instead of 16 for gen files and run ids

--- .github/scripts/base62.sh
+++ .github/scripts/base62.sh
@@ -3,13 +3,13 @@
 set -o errexit
 set -o pipefail
 
-# Convert the given number to base62 characters
+# Convert the given Base 10 number to Base 58 characters
 # ex. base62.sh 1718738365297350992 
-# ex. base62.sh $(date +%s%N) 
+# ex. ./base62.sh $(date +%s%03N) 
 
 DECNUM=$1
+BASE62=($(echo {0..9} {A..Z} {a..z}))
 
-BASE62=($(echo {0..9} {a..z} {A..Z}))
 for i in $(bc <<< "obase=62; ${DECNUM}"); do
   echo -n ${BASE62[$(( 10#$i ))]}
 done && echo
--- .github/scripts/fetch-results-local.sh
+++ .github/scripts/fetch-results-local.sh
@@ -17,7 +17,7 @@ USER=$2
 SCRIPT_DIR=$3
 RUN_TYPE=$4
 ACTOR=$5
-RUN_LABEL=${6:-$(echo -n "set-"; ${SCRIPT_DIR}/base62.sh $(date +%s%N))}
+RUN_LABEL=${6:-$(echo -n "set-"; ${SCRIPT_DIR}/base62.sh $(date +%s%03N))}
 RUN_DIR=/root/run
 
 # Pull results from the benchmark server
