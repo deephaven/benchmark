d7b76fdab567bb87b673ebf77f40eee0f3a7390c
stanbrub <stanbrubaker@illumon.com>
2024-07-29 21:14:03 -0600

    First pass at workflow changes for doing 1 job per iteration

---
+++ .github/scripts/matrix_array.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+# For a given descriptor, return the appropriate matrix array
+# that can be read in a Github workflow with fromJSON(array) that 
+# corresponds 
+# ex. matrix_array.sh adhoc 5
+# ex. matrix_array.sh release 4
+
+NAME=$1
+RUN_TYPE=$2
+ITERATIONS=$(($3 - 1))
+
+STR='["Any"'
+TAG='Any'
+
+if [ "${RUN_TYPE}" = 'release' ] || [ "${RUN_TYPE}" = 'nigthly' ]; then
+  TAG='Iterate' 
+fi
+
+for i in $(seq ${ITERATIONS}); do
+  STR=${STR}',"'${TAG}'"'
+done
+
+STR=${STR}']'
+
+echo "${NAME}=${STR}"
+
+
