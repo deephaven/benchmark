c7a720352ebf1591a2c5520aca1fb5a487f272a0
stanbrub <stanbrubaker@illumon.com>
2024-10-08 15:28:03 -0600

    Fixed the source of the distro so that older release workflows can work

--- .github/scripts/make-release-distro.sh
+++ .github/scripts/make-release-distro.sh
@@ -17,10 +17,11 @@ fi
 RELEASE_VERSION=$1
 RELEASE_COMMIT=$2
 PREVIOUS_VERSION=$3
+DISTRO_SOURCE=$4
 RELEASE_TAG="v${RELEASE_VERSION}"
 PREVIOUS_TAG="v${PREVIOUS_VERSION}"
 ARTIFACT=deephaven-benchmark-${RELEASE_VERSION}
-DISTRO=target/distro
+DISTRO_DEST=target/distro
 THIS=$(basename "$0")
 RELEASE_NOTES=target/release-notes.md
 
@@ -30,13 +31,13 @@ git log --oneline ${PREVIOUS_TAG}...${RELEASE_COMMIT} | sed -e 's/^/- /' >> ${RE
 echo "**Full Changelog**: https://github.com/deephaven/benchmark/compare/${PREVIOUS_TAG}...${RELEASE_TAG}" >> ${RELEASE_NOTES}
 
 # Build the Distro for running standard benchmarks
-mkdir -p ${DISTRO}/libs/
-cp .github/distro/* ${DISTRO}
-cp target/dependencies/* ${DISTRO}/libs
-cp target/deephaven-benchmark-1.0-SNAPSHOT.jar ${DISTRO}/libs/${ARTIFACT}.jar
-cp target/deephaven-benchmark-1.0-SNAPSHOT-tests.jar ${DISTRO}/libs/${ARTIFACT}-tests.jar
-echo "VERSION=${RELEASE_VERSION}" > ${DISTRO}/.env
+mkdir -p ${DISTRO_DEST}/libs/
+cp ${DISTRO_SOURCE}/* ${DISTRO_DEST}
+cp target/dependencies/* ${DISTRO_DEST}/libs
+cp target/deephaven-benchmark-1.0-SNAPSHOT.jar ${DISTRO_DEST}/libs/${ARTIFACT}.jar
+cp target/deephaven-benchmark-1.0-SNAPSHOT-tests.jar ${DISTRO_DEST}/libs/${ARTIFACT}-tests.jar
+echo "VERSION=${RELEASE_VERSION}" > ${DISTRO_DEST}/.env
 
-cd ${DISTRO}
+cd ${DISTRO_DEST}
 tar cvzf ../${ARTIFACT}.tar * .env
 
