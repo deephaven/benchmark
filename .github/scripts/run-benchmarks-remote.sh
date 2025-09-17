f85c9b219630a8ef656fc0ddb1f42d0150712441
stanbrub <stanbrubaker@illumon.com>
2025-09-16 18:55:02 -0600

    Debug code

--- .github/scripts/run-benchmarks-remote.sh
+++ .github/scripts/run-benchmarks-remote.sh
@@ -14,7 +14,7 @@ set -o nounset
 
 if [[ $# != 6 ]]; then
   echo "$0: Missing run type, test package, test classes, row count, distribution, or tag name"
-  for i in "${!@}"; do
+  for ((i = 1; i <= $#; i++)); do
     echo "Argument $i: ${!i}"
   done
   exit 1
